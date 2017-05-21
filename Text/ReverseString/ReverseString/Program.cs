43f72cdc5c9e39e94a087973d3183bc698e508df
lproductions <l.productionsuk@gmail.com>
2017-05-21 17:38:46 +0100

    Add two new projects in Text

---
+++ Text/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString {
+    class Program {
+        static void Main(string[] args) {
+            string input = Console.ReadLine();
+            
+            int j = 0;
+            StringBuilder sb = new StringBuilder(input);
+            
+            for(int i = input.Length -1; i>=0; i--){
+                
+                sb[j] = input[i];
+                j++;
+
+            }
+            Console.Out.WriteLine(sb.ToString());
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Text/fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz {
+    class Program {
+        static void Main(string[] args) {
+            for (int i = 1; i < 101; i++) {
+                string output = "" + i;
+                if (i % 3 == 0) {
+                    output = "Fizz";
+                }
+                if (i % 5 == 0) {
+                    output = "Buzz";
+                }
+                if (i % 5 == 0 && i % 3 == 0) {
+                    output = "FizzBuzz";
+                }
+                Console.Out.WriteLine(output);
+
+            }
+            Console.Read();
+        }
+    }
+}
